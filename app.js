ad4b860c3302c937463f8335a8a62da942fd53bc
vanshika-sood <vanshika29sood@gmail.com>
2022-02-10 12:29:02 +0530

    modified app.js

--- app.js
+++ app.js
@@ -71,11 +71,7 @@ app.get(
   })
 );
 
-// GET /auth/spotify/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request. If authentication fails, the user will be redirected back to the
-//   login page. Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
+
 app.get(
   '/auth/spotify/secrets',
   passport.authenticate('spotify', {failureRedirect: '/'}),
@@ -90,11 +86,6 @@ app.listen(port, function () {
   console.log('App is listening on port ' + port);
 });
 
-// Simple route middleware to ensure user is authenticated.
-//   Use this route middleware on any resource that needs to be protected.  If
-//   the request is authenticated (typically via a persistent login session),
-//   the request will proceed. Otherwise, the user will be redirected to the
-//   login page.
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
